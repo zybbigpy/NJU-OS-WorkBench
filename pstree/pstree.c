d5391aff260b58a1d9844587a19dd0168d4ebf70
tracer-nju <tracer@nju.edu.cn>
2019-03-05 00:30:50 -0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 389327349ab544ff8544a6abe22242b5            Boot ID: 274c23c8e835471b8ba63e72b4cf080f     Virtualization: vmware   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  00:30:50 up  5:22,  1 user,  load average: 0.33, 0.43, 0.37

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -26,7 +26,7 @@ ProcInfo sys_porcs[MAX_PROC_NUM];
 /* the file addr is always /proc/[pid]/stat. */
 int FillSysProcInfo(const char *file_addr, int proc_index) {
 
-  // printf(" the process index in the array is %d\n", proc_index);
+  printf(" the process index in the array is %d\n", proc_index);
   FILE *fp = fopen(file_addr, "r");
   if (fp) {
     fscanf(fp, "%d%s%s%d%d", &sys_porcs[proc_index].pid,
@@ -61,7 +61,7 @@ int OpenProcDir(const char *dir_addr) {
           return 1;
         } else {
           printf("I am here and there is a bug ...\n");
-          proc_index++;
+          proc_index = proc_index + 1;
           if(proc_index == MAX_PROC_NUM) {
             perror("proc array is full!\n");
             return 1;
