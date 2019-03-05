705083e0e25cf88a28dc18b8c55c480cf05ab446
tracer-nju <tracer@nju.edu.cn>
2019-03-04 19:22:13 -0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 389327349ab544ff8544a6abe22242b5            Boot ID: 274c23c8e835471b8ba63e72b4cf080f     Virtualization: vmware   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  19:22:13 up 14 min,  1 user,  load average: 0.39, 0.26, 0.22

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,13 +1,27 @@
-#include <stdio.h>
 #include <assert.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/types.h>
 
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
-  int i;
-  for (i = 0; i < argc; i++) {
-    assert(argv[i]); // always true
-    printf("argv[%d] = %s\n", i, argv[i]);
+  // int i;
+  // for (i = 0; i < argc; i++) {
+  //   assert(argv[i]); // always true
+  //   printf("argv[%d] = %s\n", i, argv[i]);
+  // }
+  // assert(!argv[argc]); // always true
+  // return 0;
+  DIR *dir;
+  struct dirent *ptr;
+  dir = opendir("/proc");
+  if (dir) {
+    while ((ptr = readdir(dir)) != NULL) {
+      printf("dir name is:%s \n ", ptr->d_name);
+    }
+  } else {
+    perror("open dir fail\n");
   }
-  assert(!argv[argc]); // always true
+
   return 0;
 }
