6fa309c39c78bd8aa18e30e455d835ec4072934b
tracer-nju <tracer@nju.edu.cn>
2019-03-04 21:35:00 -0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 389327349ab544ff8544a6abe22242b5            Boot ID: 274c23c8e835471b8ba63e72b4cf080f     Virtualization: vmware   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-15-generic       Architecture: x86-64  21:35:00 up  2:26,  1 user,  load average: 0.32, 0.18, 0.07

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,9 +2,49 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <string.h>
+
+#define MAX_PROC_NUM 2048
+
+/*   the process information is in /proc/[pid]/stat.
+**   ref: man 5 proc
+*/
+typedef struct ProcInfo {
+  pid_t pid;
+  char comm[20];
+  char state;
+  pid_t ppid;
+  pid_t pgrp;
+} ProcInfo;
+
+ProcInfo sys_porcs[MAX_PROC_NUM];
+
+int FillSysProcInfo(const char *file_addr) {
+  /* the file addr is always /proc/[pid]/stat. */
+}
+
+int OpenProcDir(const char *dir_addr) {
+  DIR *dir;
+  struct dirent *ptr;
+  dir = opendir(dir_addr);
+  if (dir) {
+    while ((ptr = readdir(dir)) != NULL) {
+      /* create process information */
+      // printf("dir name is:%s \n ", ptr->d_name);
+      char file_addr[30];
+      sprintf(file_addr, "%s%s%s", dir_addr, ptr->d_name, "/stat");
+      printf("the file name is %s",file_addr);
+      //int ret = FillSysProcInfo(dir_addr);
+    }
+  } else {
+    perror("open dir fail\n");
+    return 1;
+  }
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
-  printf("Hello, World!\n");
+  // printf("Hello, World!\n");
   // int i;
   // for (i = 0; i < argc; i++) {
   //   assert(argv[i]); // always true
@@ -12,16 +52,5 @@ int main(int argc, char *argv[]) {
   // }
   // assert(!argv[argc]); // always true
   // return 0;
-  DIR *dir;
-  struct dirent *ptr;
-  dir = opendir("/proc");
-  if (dir) {
-    while ((ptr = readdir(dir)) != NULL) {
-      printf("dir name is:%s \n ", ptr->d_name);
-    }
-  } else {
-    perror("open dir fail\n");
-  }
-
   return 0;
 }
