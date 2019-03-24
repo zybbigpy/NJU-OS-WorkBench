0b91b7b6111bdd098a06803b78aed81901515de3
tracer-nju <tracer@nju.edu.cn>
2019-03-24 04:35:35 -0700

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 389327349ab544ff8544a6abe22242b5            Boot ID: 14d81573c1724e3897d205164e48c7ec     Virtualization: vmware   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  04:35:35 up  6:00,  1 user,  load average: 1.29, 0.58, 1.18

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -19,7 +19,7 @@ void read_key() {
   _DEV_INPUT_KBD_t event = {.keycode = _KEY_NONE};
 #define KEYNAME(key) [_KEY_##key] = #key,
   // static const char *key_names[] = {_KEYS(KEYNAME)};
-  static const char *key[] = {"a", "b"};
+  static const char *key[] = {[0] = "a", [1] = "b"};
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
   if (event.keycode != _KEY_NONE && event.keydown) {
     puts("Key pressed: ");
