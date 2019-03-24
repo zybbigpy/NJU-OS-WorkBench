ed0ccf316461d73c3557aefcb0e8eedcd5d654d9
tracer-nju <tracer@nju.edu.cn>
2019-03-24 07:10:52 -0700

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 389327349ab544ff8544a6abe22242b5            Boot ID: 0149d8025bad44f2a19df1863ec4589f     Virtualization: vmware   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  07:10:53 up 51 min,  1 user,  load average: 0.54, 0.39, 0.21

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -4,6 +4,31 @@ void init_screen();
 void splash();
 void read_key();
 
+int main_rect_x = 0;
+int main_rect_y = 0;
+int dx = 5;
+int dy = 5;
+
+void draw_rect(int x, int y, int w, int h, uint32_t color) {
+  uint32_t pixels[w * h];  // WARNING: allocated on stack
+  _DEV_VIDEO_FBCTL_t event = {
+      .x = x,
+      .y = y,
+      .w = w,
+      .h = h,
+      .sync = 1,
+      .pixels = pixels,
+  };
+  for (int i = 0; i < w * h; i++) {
+    pixels[i] = color;
+  }
+  _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &event, sizeof(event));
+}
+
+void show_main_rect(uint32_t color) {
+  draw_rect(main_rect_x * SIDE, main_rect_y * SIDE, SIDE, SIDE, color);
+}
+
 int main() {
   // Operating system is a C program
   _ioe_init();
@@ -22,12 +47,29 @@ void read_key() {
   static const char *key_names[] = {_KEYS(KEYNAME)};
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
   if (event.keycode != _KEY_NONE && event.keydown) {
-    if (event.keycode == _KEY_UP) {
-      splash(0xffffffff);
+    // if (event.keycode == _KEY_UP) {
+    //   splash(0xffffffff);
+    // }
+    // if (event.keycode == _KEY_DOWN) {
+    //   splash(0);
+    // }
+    switch (event.keycode) {
+      case _KEY_UP:
+        main_rect_y += dx;
+        break;
+      case _KEY_DOWN:
+        main_rect_y -= dy;
+        break;
+      case _KEY_LEFT:
+        main_rect_x -= dx;
+        break;
+      case _KEY_RIGHT:
+        main_rect_x += dx;
+        break;
+      default:
+        break;
     }
-    if (event.keycode == _KEY_DOWN) {
-      splash(0);
-    }
+    show_main_rect(0xffffffff);
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
@@ -43,28 +85,12 @@ void init_screen() {
   h = info.height;
 }
 
-void draw_rect(int x, int y, int w, int h, uint32_t color) {
-  uint32_t pixels[w * h];  // WARNING: allocated on stack
-  _DEV_VIDEO_FBCTL_t event = {
-      .x = x,
-      .y = y,
-      .w = w,
-      .h = h,
-      .sync = 1,
-      .pixels = pixels,
-  };
-  for (int i = 0; i < w * h; i++) {
-    pixels[i] = color;
-  }
-  _io_write(_DEV_VIDEO, _DEVREG_VIDEO_FBCTL, &event, sizeof(event));
-}
-
-void splash(uint32_t color) {
-  for (int x = 0; x * SIDE <= w; x++) {
-    for (int y = 0; y * SIDE <= h; y++) {
-      if ((x & 1) ^ (y & 1)) {
-        draw_rect(x * SIDE, y * SIDE, SIDE, SIDE, color);  // white
-      }
-    }
-  }
-}
+// void splash(uint32_t color) {
+//   for (int x = 0; x * SIDE <= w; x++) {
+//     for (int y = 0; y * SIDE <= h; y++) {
+//       if ((x & 1) ^ (y & 1)) {
+//         draw_rect(x * SIDE, y * SIDE, SIDE, SIDE, color);  // white
+//       }
+//     }
+//   }
+// }
