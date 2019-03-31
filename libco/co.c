1c014242268561eceb0382fea44676df75fbc001
tracer-nju <tracer@nju.edu.cn>
2019-03-31 17:16:29 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: df4da1e8ed6c47f0a07fb7e7790cdb64   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  17:16:29 up 3 days, 20:19,  1 user,  load average: 0.64, 0.64, 0.63

--- libco/co.c
+++ libco/co.c
@@ -1,9 +1,15 @@
 #include "co.h"
+#include <assert.h>
 #include <setjmp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
+
+#if defined(__i386__)
+define SP "%%esp"
+#elif defined(__x86_64__)
+#define SP "%%rsp"
+#endif
 
 #define STACK_SIZE 4086
 #define CO_NUM_MAX 15
@@ -12,7 +18,8 @@
 #define END 1
 #define YIELD 2
 
-struct co {
+    struct co
+{
   char name[10];
   int id;
 
@@ -24,19 +31,23 @@ struct co {
   struct co *next;
 
   jmp_buf ctx;
-};  // CO definition
+}; // CO definition
 
-jmp_buf main_ctx;             // main jmp ctx
-static struct co *coroutins;  // use linked list to store coroutines
-static struct co *current;    // current coroutine
-static int co_no = 0;         // total co number
+jmp_buf main_ctx;            // main jmp ctx
+static struct co *coroutins; // use linked list to store coroutines
+static struct co *current;   // current coroutine
+static int co_no = 0;        // total co number
+void *__stack_backup;
+int num =0 ;
 
-void co_init() {
+void co_init()
+{
   coroutins = NULL;
   current = NULL;
 }
 
-struct co *co_start(const char *name, func_t func, void *arg) {
+struct co *co_start(const char *name, func_t func, void *arg)
+{
   struct co *co = (struct co *)malloc(sizeof(struct co));
   co->args = arg;
   co->func = func;
@@ -48,10 +59,13 @@ struct co *co_start(const char *name, func_t func, void *arg) {
   assert(co_no < CO_NUM_MAX);
 
   // add co into in the linked list
-  if (!coroutins) {
+  if (!coroutins)
+  {
     co->prev = co;
     co->next = co;
-  } else {
+  }
+  else
+  {
     co->prev = coroutins->prev;
     co->next = coroutins;
     coroutins->prev->next = co;
@@ -59,52 +73,75 @@ struct co *co_start(const char *name, func_t func, void *arg) {
   }
   coroutins = co;
 
-  if (setjmp(co->ctx)) {
-    func(arg);
-    longjmp(main_ctx, END);
+  if (setjmp(co->ctx))
+  {
+    // mov esp or rsp
+    if ()
+    {
+      asm volatile("mov " SP ", %0; mov %1, " SP
+                   : "=g"(__stack_backup)
+                   : "g"(co->stack + sizeof(STACK_SIZE)));
+      func(arg);
+      longjmp(main_ctx, END);
+    }
   }
-  return co;
+  else
+  {
+    return co;
+  }
 }
 
-void co_destroy(struct co *thd) {
+void co_destroy(struct co *thd)
+{
   free(thd->stack);
   free(thd);
 }
 
-void co_wait(struct co *thd) {
-  if (coroutins == NULL) return;
+void co_wait(struct co *thd)
+{
+  if (coroutins == NULL)
+    return;
 
   struct co *co_;
-  switch (setjmp(main_ctx)) {
-    case INIT:
-      current = coroutins;
-      break;
+  switch (setjmp(main_ctx))
+  {
+  case INIT:
+    current = coroutins;
+    break;
 
-    case YIELD:
-      current = current->next;
-      break;
+  case YIELD:
+    current = current->next;
+    break;
 
-    case END:
-      co_ = current;
-      if (co_->next == co_) {
-        coroutins = NULL;
-        co_destroy(co_);
-        return;
-      }
-      current = current->next;
-      co_->prev->next = co_->next;
-      co_->next->prev = co_->prev;
+  case END:
+    co_ = current;
+    if (co_->next == co_)
+    {
+      coroutins = NULL;
       co_destroy(co_);
-      break;
+      asm volatile("mov %0," SP
+                   :
+                   : "g"(__stack_backup));
+      return;
+    }
+    current = current->next;
+    co_->prev->next = co_->next;
+    co_->next->prev = co_->prev;
+    co_destroy(co_);
+    break;
   }
   assert(current);
   longjmp(current->ctx, 1);
 }
 
-void co_yield() {
-  if (setjmp(current->ctx)) {
+void co_yield()
+{
+  if (setjmp(current->ctx))
+  {
     return;
-  } else {
+  }
+  else
+  {
     longjmp(main_ctx, YIELD);
   }
 }
