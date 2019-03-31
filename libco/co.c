b80d988eaf589bb3a7afb7a616a3532072859000
tracer-nju <tracer@nju.edu.cn>
2019-03-31 18:58:50 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: df4da1e8ed6c47f0a07fb7e7790cdb64   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  18:58:50 up 3 days, 22:01,  1 user,  load average: 1.16, 1.07, 0.69

--- libco/co.c
+++ libco/co.c
@@ -52,11 +52,11 @@ void co_init()
   current = NULL;
 }
 
-static void start_co()
-{
-  current->func(current->args);
-  longjmp(main_ctx, END);
-}
+// static void start_co()
+// {
+//   current->func(current->args);
+//   longjmp(main_ctx, END);
+// }
 
 struct co *co_start(const char *name, func_t func, void *arg)
 {
@@ -101,10 +101,10 @@ struct co *co_start(const char *name, func_t func, void *arg)
     // asm volatile("mov " PC ", %0; mov %1, " SP
     //              : "=g"(co->__pc_backup)
     //              : "g"((void *)start_co));
-    // func(arg);
-     start_co();
+    func(arg);
+    // start_co();
     // asm volatile("mov %0," SP : : "g"(co->__stack_backup));
-    // longjmp(main_ctx, END);
+     longjmp(main_ctx, END);
   }
   return co;
 }
