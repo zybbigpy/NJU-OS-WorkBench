d7a1a14597d70520f64856872310e2f48b84e027
tracer-nju <tracer@nju.edu.cn>
2019-04-01 09:07:09 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: df4da1e8ed6c47f0a07fb7e7790cdb64   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-16-generic       Architecture: x86-64  09:07:09 up 4 days, 12:10,  1 user,  load average: 0.92, 0.95, 0.85

--- libco/co.c
+++ libco/co.c
@@ -72,7 +72,8 @@ static void co_init_(struct co *co) {
   asm volatile("mov " SP ", %0; mov %1, " SP
                : "=g"(co->__stack_backup)
                : "g"(co->stack + STACK_SIZE));
-  printf("init co [%s], SP is [%p] \n", co->name, co->stack + STACK_SIZE);
+  //printf("init co [%s], SP is [%p] \n", co->name, co->stack + STACK_SIZE);
+  printf("init co[%s]",co->name);
   co->func(co->args);
   // asm volatile("mov %0," SP : : "g"(co->__stack_backup));
   longjmp(main_ctx, END);
