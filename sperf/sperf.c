eb925e8aec6a59068a9482e21edfefd2890a8526
tracer-nju <tracer@nju.edu.cn>
2019-04-08 23:23:24 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: b2e31ce2b47a492796d0cbdfd9e63bd0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  23:23:24 up  4:43,  1 user,  load average: 0.46, 0.62, 0.88

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -28,7 +28,7 @@ int pipefd[2];
 // child process
 void child_proc(int argc, char *argv[]) {
   // redirect stdout
-  //close(pipefd[0]);
+  close(pipefd[0]);
   dup2(pipefd[1], STDERR_FILENO);
   
   // close(pipefd[1]);
@@ -49,7 +49,7 @@ void child_proc(int argc, char *argv[]) {
 // parent process
 void parent_proc() {
   // redirect stdin
-  //close(pipefd[1]);
+  close(pipefd[1]);
   dup2(pipefd[0], STDIN_FILENO);
   // close(pipefd[0]);
   
