c3a291fd01f76e84a82873b38c581cbc27e994da
tracer-nju <tracer@nju.edu.cn>
2019-04-08 20:10:30 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: b2e31ce2b47a492796d0cbdfd9e63bd0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  20:10:30 up  1:30,  1 user,  load average: 1.26, 0.90, 0.83

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,3 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void error(char *msg) {
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
+
+int pipefd[2];
+void child_proc(char *args[]) {
+  dup2(pipefd[1], STDOUT_FILENO);
+  close(pipefd[0]);
+  close(pipefd[1]);
+  execvp("/usr/bin/strace", args);
+}
+void parent_proc() {
+  dup2(pipefd[1], STDIN_FILENO);
+  close(pipefd[0]);
+  close(pipefd[1]);
+
+  char buf[1024];
+  while(fgets(buf, 1024, stdin)) {
+    puts(buf);
+  }
+}
+
 int main(int argc, char *argv[]) {
+  pid_t pid = fork();
+  if (pid < 0) {
+    error("fork");
+  }
+  if (pid == 0) {
+    char *args[10];
+    args[0] = "strace";
+    args[1] = "-T";
+    for (int i = 1; i < argc; ++i) args[i + 1] = argv[i];
+    child_proc(args);
+  } else {
+    parent_proc();
+  }
   return 0;
 }
