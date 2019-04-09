cf3d82d16226c604a494166b87ed3dc5a108c010
tracer-nju <tracer@nju.edu.cn>
2019-04-09 15:06:22 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: b2e31ce2b47a492796d0cbdfd9e63bd0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  15:06:23 up 20:26,  1 user,  load average: 0.70, 0.98, 1.06

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -54,7 +54,7 @@ int FillSysProcInfo(
     puts(buf);
 
     char *lh = buf;
-    char *rh = buf+MAX_FILE_ADDR_LEN;
+    char *rh = buf+MAX_FILE_ADDR_LEN-1;
     while (*lh) {
       if (*lh == '(') break;
       ++lh;
