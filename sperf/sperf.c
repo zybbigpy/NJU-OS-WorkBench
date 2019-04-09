821e32ace6a09549a2474185155661ba7b4811ea
tracer-nju <tracer@nju.edu.cn>
2019-04-09 10:21:28 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: b2e31ce2b47a492796d0cbdfd9e63bd0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  10:21:28 up 15:41,  1 user,  load average: 1.03, 1.13, 1.04

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -49,8 +49,10 @@ void calculate_time_percent() {
   long long total_time = 0;
   for (int i = 0; i < syscall_num; ++i) total_time += syscalls[i].sys_time;
 
-  for (int i = 0; i < syscall_num; ++i)
+  printf("total time is %lld\n", total_time);
+  for (int i = 0; i < syscall_num; ++i) {
     syscalls[i].time_percent = syscalls[i].sys_time / total_time;
+    printf("the percentag is %f", syscalls[i].time_percent);}
 }
 
 void add_syscall(char *name, char *time) {
