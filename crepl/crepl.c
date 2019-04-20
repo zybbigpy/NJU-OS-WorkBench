485b700f05529307620a8eabed1eed919c97bf64
tracer-nju <tracer@nju.edu.cn>
2019-04-20 15:29:15 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: 15e30c587c7d4944a9a7bf6fff6180dd   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  15:29:15 up 7 days, 16:28,  1 user,  load average: 1.14, 0.86, 0.73

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,3 +1,37 @@
-int main(int argc, char *argv[]) {
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_SIZE 1024
+
+void error(const char* msg) {
+  perror(msg);
+  exit(EXIT_FAILURE);
+}
+
+int read_line(char *strin) {
+  // print promt
+  printf(">> ");
+  int ret = fgets(strin, MAX_LINE_SIZE, stdin);
+  if(ret == 0) {
+    error("fgets()");
+  }
+  return ret;
+}
+
+int is_func() {}
+
+int is_expr() {}
+
+int main() {
+  char strin[MAX_LINE_SIZE] = {0};
+  while (read_line(strin)) {
+    if (strcmp(strin, "exit\n")) {
+      exit(EXIT_SUCCESS);
+    } else if (strcmp(strin, "\n")) {
+      continue;
+    } else if (is_func(strin)) {
+    } else if (is_expr(strin)) {
+    }
+  }
 }
