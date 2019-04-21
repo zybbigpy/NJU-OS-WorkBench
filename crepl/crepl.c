5ee73a0294e2c3d1a02f186a123865afb3b0c512
tracer-nju <tracer@nju.edu.cn>
2019-04-21 14:50:23 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: 15e30c587c7d4944a9a7bf6fff6180dd   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-17-generic       Architecture: x86-64  14:50:23 up 8 days, 15:49,  1 user,  load average: 0.55, 0.66, 0.63

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -71,7 +71,8 @@ void compute(const char* strin) {}
 int is_func(const char* strin) {  // only for funcs like int func();
   printf("the strin is %s\n", strin);
   char prefix[] = "int ";
-  printf(" the prefix len is %d\n",strlen(prefix));
+  size_t len = strlen(prefix);
+  printf(" the prefix len is %d\n",len);
 
   int ret = 0;
   if (strncmp(strin, prefix, strlen(prefix)) == 0) ret = 1;
