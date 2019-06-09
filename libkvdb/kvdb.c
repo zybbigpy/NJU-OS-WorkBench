46e2cdfcf94ffffadd08be99ba4d4779b79ddc18
tracer-nju <tracer@nju.edu.cn>
2019-06-09 19:40:21 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: 6b57060f3b60402eba1827caacae725b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  19:40:21 up 1 day,  4:54,  1 user,  load average: 2.00, 2.30, 2.02

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -13,8 +13,10 @@ char *kvdb_get_thread_unsafe(kvdb_t *db, const char *key);
 int kvdb_lock(kvdb_t *db);
 int kvdb_unlock(kvdb_t *db);
 int kvdb_check(kvdb_t *db);
+int kvdb_recover(kvdb_t *db);
 
 void log_error(char *msg) { perror(msg); }
+void log_debug(char *msg) { perror(msg); }
 
 // thread safe kvdb open
 int kvdb_open(kvdb_t *db, const char *filename) {
@@ -42,11 +44,9 @@ int kvdb_put(kvdb_t *db, const char *key, const char *value) {
 
 // thread safe kvdb get
 char *kvdb_get(kvdb_t *db, const char *key) {
-
-  // there is no need to add mutex for read
-  // pthread_mutex_lock(&db->thread_lock);
+  pthread_mutex_lock(&db->thread_lock);
   char *ret = kvdb_get_thread_unsafe(db, key);
-  // pthread_mutex_unlock(&db->thread_lock);
+  pthread_mutex_unlock(&db->thread_lock);
   return ret;
 }
 
@@ -146,6 +146,19 @@ int kvdb_close_thread_unsafe(kvdb_t *db) {
   return 0;
 }
 
+#define LOG_EPT 0
+#define LOG_BGN 1
+#define LOG_COMMIT 2
+#define LOG_FREE 3
+
+int kvdb_set_log(int log_fd, int flag) {
+  lseek(log_fd, 0, SEEK_SET);
+  if (write(log_fd, &flag, sizeof(flag)) != sizeof(flag)) {
+    return -1;
+  }
+  return 0;
+}
+
 int kvdb_put_thread_unsafe(kvdb_t *db, const char *key, const char *value) {
   kvdb_lock(db);
   assert(db);
@@ -153,30 +166,81 @@ int kvdb_put_thread_unsafe(kvdb_t *db, const char *key, const char *value) {
   assert(db->db_log);
 
   int file_fd = db->db_file->_fileno;
-  // int log_fd = db->db_log->_fileno;
+  int log_fd = db->db_log->_fileno;
 
-  // log begin
-
-  // log commit
-
-  // db write
   size_t key_size = strlen(key);
   size_t val_size = strlen(value);
+
+  // log begin
+  if (kvdb_set_log(log_fd, LOG_BGN) != 0) {
+    log_error("log bgn write error.\n");
+    kvdb_unlock(db);
+    return -1;
+  }
+  fsync(log_fd);
+
+  // do logging
+  int offset = lseek(file_fd, 0, SEEK_END);
+  if (offset == -1) {
+    kvdb_unlock(db);
+    return -1;
+  }
+  if (write(log_fd, &offset, sizeof(offset)) != sizeof(offset)) {
+    kvdb_unlock(db);
+    return -1;
+  }
+  if (write(log_fd, &key_size, sizeof(key_size)) != sizeof(key_size)) {
+    kvdb_unlock(db);
+    return -1;
+  }
+  if (write(log_fd, &val_size, sizeof(val_size)) != sizeof(val_size)) {
+    kvdb_unlock(db);
+    return -1;
+  }
+  if (write(log_fd, key, strlen(key)) != strlen(key)) {
+    kvdb_unlock(db);
+    return -1;
+  }
+  if (write(log_fd, value, strlen(value)) != strlen(value)) {
+    kvdb_unlock(db);
+    return -1;
+  }
+  fsync(log_fd);
+
+  // log commit
+  if (kvdb_set_log(log_fd, LOG_COMMIT) != 0) {
+    log_error("log commit write error.\n");
+    kvdb_unlock(db);
+    return -1;
+  }
+  fsync(log_fd);
+
+  // db write
   if (write(file_fd, &key_size, sizeof(key_size)) != sizeof(key_size)) {
+    kvdb_unlock(db);
     return -1;
   }
   if (write(file_fd, &val_size, sizeof(val_size)) != sizeof(val_size)) {
+    kvdb_unlock(db);
     return -1;
   }
   if (write(file_fd, key, strlen(key)) != strlen(key)) {
+    kvdb_unlock(db);
     return -1;
   }
   if (write(file_fd, value, strlen(value)) != strlen(value)) {
+    kvdb_unlock(db);
     return -1;
   }
   fsync(file_fd);
 
-  // log
+  // log free
+  if (kvdb_set_log(log_fd, LOG_FREE) != 0) {
+    log_error("log free write error.\n");
+    kvdb_unlock(db);
+    return -1;
+  }
+  fsync(log_fd);
 
   kvdb_unlock(db);
   return 0;
@@ -186,6 +250,8 @@ char *kvdb_get_thread_unsafe(kvdb_t *db, const char *key) {
   assert(db);
   assert(db->db_file);
 
+  kvdb_lock(db);
+
   int file_fd = db->db_file->_fileno;
   size_t key_size = 0;
   size_t val_size = 0;
@@ -194,6 +260,7 @@ char *kvdb_get_thread_unsafe(kvdb_t *db, const char *key) {
 
   if (lseek(file_fd, 0, SEEK_SET) == -1) {
     log_error("error in get_lseek.\n");
+    kvdb_unlock(db);
     return NULL;
   }
 
@@ -201,16 +268,19 @@ char *kvdb_get_thread_unsafe(kvdb_t *db, const char *key) {
     ssize_t read_ret = read(file_fd, &key_size, sizeof(key_size));
     if (read_ret == 0) {
       log_error("can not find the key in db. \n");
+      kvdb_unlock(db);
       return NULL;
     }
 
     if (read_ret != sizeof(key_size)) {
       log_error("read key size error in get.\n");
+      kvdb_unlock(db);
       return NULL;
     }
 
     if (read(file_fd, &val_size, sizeof(val_size)) != sizeof(val_size)) {
       log_error("read val size error in get. \n");
+      kvdb_unlock(db);
       return NULL;
     }
 
@@ -225,6 +295,7 @@ char *kvdb_get_thread_unsafe(kvdb_t *db, const char *key) {
       log_error("read key error in get. \n");
       free(val_buf);
       free(key_buf);
+      kvdb_unlock(db);
       return NULL;
     }
 
@@ -232,6 +303,7 @@ char *kvdb_get_thread_unsafe(kvdb_t *db, const char *key) {
       log_error("read val error in get. \n");
       free(val_buf);
       free(val_buf);
+      kvdb_unlock(db);
       return NULL;
     }
 
@@ -246,5 +318,60 @@ char *kvdb_get_thread_unsafe(kvdb_t *db, const char *key) {
 
   assert(key_buf != NULL);
   free(key_buf);
+  kvdb_unlock(db);
   return val_buf;
+}
+
+int kvdb_check(kvdb_t *db) {
+  assert(db);
+  assert(db->db_log);
+
+  int log_fd = db->db_log->_fileno;
+  if (lseek(log_fd, 0, SEEK_SET) == -1) {
+    return -1;
+  }
+
+  int flag = 0;
+  flag = read(log_fd, &flag, sizeof(flag));
+
+  if (flag == LOG_BGN) {
+    log_error("lose last record.\n");
+    return 0;
+  }
+  if (flag == LOG_COMMIT) {
+    log_debug("begin recover the db.\n");
+    if (kvdb_recover(db) != 0) {
+      log_error("error in db recovery. \n");
+      return -1;
+    }
+    return 0;
+  }
+}
+
+int kvdb_recover(kvdb_t *db) {
+  assert(db);
+  assert(db->db_log);
+  assert(db->db_file);
+
+  int log_fd = db->db_file->_fileno;
+  int file_fd = db->db_log->_fileno;
+
+  int flag;
+  int offset;
+  int key_size, val_size;
+  char *buf;
+  lseek(log_fd, 0, SEEK_SET);
+  read(log_fd, &flag, sizeof(flag));
+  read(log_fd, &offset, sizeof(offset));
+  read(log_fd, &key_size, sizeof(key_size));
+  read(log_fd, &val_size, sizeof(val_size));
+  buf = (char *)malloc(val_size + key_size);
+  lseek(file_fd, offset, SEEK_SET);
+  write(file_fd, buf, key_size + val_size);
+  fsync(file_fd);
+
+  kvdb_set_log(log_fd, LOG_FREE);
+
+  free(buf);
+  return 0;
 }
