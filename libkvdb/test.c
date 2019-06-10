5999aa93cb9054f6f56f81b2c378c837ae2c7401
tracer-nju <tracer@nju.edu.cn>
2019-06-10 10:14:08 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: 6b57060f3b60402eba1827caacae725b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  10:14:08 up 1 day, 19:28,  1 user,  load average: 1.05, 0.82, 0.66

--- libkvdb/test.c
+++ libkvdb/test.c
@@ -4,9 +4,12 @@
 
 int main() {
     kvdb_t db;
+    char* value;
     kvdb_open(&db, "good.db");
     kvdb_put(&db, "good", "try");
+    kvdb_get(&db, "good");
 
+    printf("key is %s, and the value is %s .\n", "good", value);
     kvdb_close(&db);
     return 0;
 }
