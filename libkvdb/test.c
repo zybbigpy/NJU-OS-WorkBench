24e9c5e0865751b65efc5398b434fbf7510b0767
tracer-nju <tracer@nju.edu.cn>
2019-06-10 10:24:28 +0800

    Static hostname: wangqian-ThinkPad-E460          Icon name: computer-laptop            Chassis: laptop         Machine ID: af9a9a1e8ab44f989343cd8bfbd330d8            Boot ID: 6b57060f3b60402eba1827caacae725b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.18.0-21-generic       Architecture: x86-64  10:24:28 up 1 day, 19:38,  1 user,  load average: 0.64, 0.70, 0.68

--- libkvdb/test.c
+++ libkvdb/test.c
@@ -8,10 +8,15 @@ int main() {
     
     kvdb_open(&db, "good.db");
     kvdb_put(&db, "good", "try");
-    char* value= kvdb_get(&db, "good");
+    kvdb_put(&db, "jyy", "a-good-guy");
+    char* value1 = kvdb_get(&db, "good");
+    char* value2 = kvdb_get(&db, "jyy");
 
-    printf("key is %s, and the value is %s .\n", "good", value);
-    free(value);
+    printf("key is %s, and the value is %s.\n", "good", value1);
+    printf("key is %s, and the value is %s.\n", "good", value2);
+
+    free(value1);
+    free(value2);
     kvdb_close(&db);
     return 0;
 }
